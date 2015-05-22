3a5599be595ccc059054e00fb9e4fc85430c95a7
WebRainCoder <webraincoder@gmail.com>
2015-05-22 21:54:25 +0300

    fix the site rating

--- js/script.js
+++ js/script.js
@@ -181,7 +181,7 @@ var clickBar = function(origin1,origin2,distance,direct,degNum){
 }
 
 // setFunctionForNavButton
-$('.mainNav >div:nth-child(1)').click(function(){$this = this; clickBar('50%','50%', 0, '','0deg')});
+$('.mainNav >div:nth-child(1)').click(function(){$this=this; clickBar('50%','50%', 0, '','0deg')});
 $('.mainNav >div:nth-child(2)').click(function(){$this = this; clickBar('100%','0%', '-'+height+'', 'Y','90deg')});
 $('.mainNav >div:nth-child(3)').click(function(){$this = this; clickBar('0%','0%', '-'+height+'', 'Y','-90deg')});
 $('.mainNav >div:nth-child(4)').click(function(){$this = this; clickBar('0%','0%', '-'+height+'', 'X','90deg')});
@@ -397,9 +397,15 @@ function(){
 ======				comment: Google Analytics   ========
 ========================================================
 */
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+(function(i,s,o,g,r,a,m){
+	i['GoogleAnalyticsObject']=r;i[r]=i[r] || function(){
+		(i[r].q=i[r].q||[]).push(arguments)
+	},i[r].l=1*new Date();
+	a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];
+	a.async=1;
+	a.src=g;
+	m.parentNode.insertBefore(a,m)
 })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-62902606-1', 'auto');
